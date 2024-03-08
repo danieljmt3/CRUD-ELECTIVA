0f24543929f1e6e3d5222ca9eb40d00b1e76cbc9
danielRam0s <dramosm21@curnvirtual.edu.co>
2024-03-08 13:53:45 -0500

    EQ-RAMOS: Añadir archivos de solicitud

---
+++ public/javascript/index.js
@@ -0,0 +1,24 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var titulo = document.getElementById('titulo').value;
+    var descripcion = document.getElementById('descripcion').value;
+
+    var formData = new FormData();
+    formData.append('titulo', titulo);
+    formData.append('descripcion', descripcion);
+
+    fetch('../controladores/crear.task.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message)
+            alert(data.message)
+            document.querySelector('form').reset();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+});
--- vistas/crud-tareas.php
+++ vistas/crud-tareas.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tareas CRUD</title>
     <link rel="stylesheet" href="../public/estilos/index.css">
 </head>
+
 <body>
     <section>
         <div class="formulario">
-            <form action="registrar.php">
-                <input class="title" type="text" name="title" id="title">
-                <input class="description" type="text" name="title" id="title">
+            <form>
+                <input class="title" type="text" name="titulo" id="titulo" placeholder="Ingresa el titulo de la tarea">
+                <input class="description" type="text" name="descripcion" id="descripcion" placeholder="Agrega una descripcion">
                 <div class="cont-btn">
                     <button type="submit">Agregar + </button>
                 </div>
@@ -21,11 +23,14 @@
             <div class="task">
                 <p>pasear al perro</p>
                 <div class="cont-actions">
-                     <button type="button">Completar</button>
+                    <button type="button">Completar</button>
                     <button>Editar</button>
                 </div>
             </div>
         </div>
     </section>
+
+    <script src="../public/javascript/index.js"></script>
 </body>
+
 </html>
